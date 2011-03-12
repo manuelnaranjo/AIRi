1ff609011906476c382ab3f3ab4d9f3ddf596158
Manuel Francisco Naranjo <manuel@aircable.net>
2011-03-12 17:02:25 -0300

    make media path relative to airi package

--- python/airi/server.py
+++ python/airi/server.py
@@ -10,18 +10,20 @@ from twisted.web.util import Redirect
 from airi.api import API
 from airi.stream import StreamResource, MultiPartStream
 from functools import partial
+import os
 
 __all__=['root', 'site']
+path = os.path.dirname(os.path.realpath(__file__))
 
 root = Resource()
-root.putChild("api", API())
-root.putChild("media", File("media/"))
-root.putChild("", File("media/devices.html"))
-root.putChild("scan", File("media/scan.html"))
-root.putChild("configure", File("media/configure.html"))
-root.putChild("devices", File("media/devices.html"))
-root.putChild("video", File("media/video.html"))
-root.putChild("stream", StreamResource())
+root.putChild("api",        API())
+root.putChild("media",      File(os.path.join(path, "media/")))
+root.putChild("",           File(os.path.join(path, "media/devices.html")))
+root.putChild("scan",       File(os.path.join(path, "media/scan.html")))
+root.putChild("configure",  File(os.path.join(path, "media/configure.html")))
+root.putChild("devices",    File(os.path.join(path, "media/devices.html")))
+root.putChild("video",      File(os.path.join(path, "media/video.html")))
+root.putChild("stream",     StreamResource())
 site = Site(root)
 
 if __name__=='__main__':
