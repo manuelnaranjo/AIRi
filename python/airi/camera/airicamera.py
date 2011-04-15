4b3d85b68dab49aa5ca494aaf6dee4df1c92c4cf
Manuel Francisco Naranjo <manuel@aircable.net>
2011-04-15 16:04:09 -0300

    Added set date method, fixed identation, improved function stacking

--- python/airi/camera/airicamera.py
+++ python/airi/camera/airicamera.py
@@ -3,110 +3,138 @@ from twisted.python import log
 from twisted.internet import reactor
 from airi.camera import dbg, CameraProtocol
 from airi.settings import getSettings
+from datetime import datetime
 
-settings=getSettings()
+settings = getSettings()
 
-SIZES={
+SIZES = {
   #day modes
-  "QVGA":	1,
+  "QVGA": 1,
   "VGA": 2,
   "XGA": 3,
-  "720P": 6,
   "QXGA": 5,
+  "720P": 6,
 
   "QVGA Zoom 1": 11,
   "QVGA Zoom 2": 12,
   "QVGA Zoom 3": 13,
 }
 
-PANS = [ "none", "right", "left", "up", "down" ]
+PANS = ["none", "right", "left", "up", "down"]
 
-CAPABILITIES={ 
-  'size':       SIZES.keys(),
-  'flash':      True,
-  'pan':        PANS,
-  'voice':      False, # by now ;)
-  'exposure':   True,
-  'transport':  ['RFCOMM', 'L2CAP'],
-  'battery':    True,
+CAPABILITIES = {
+  'size': [a[0] for a in sorted(SIZES.iteritems(), key=lambda(x, y):(y, x))],
+  'flash': True,
+  'pan': PANS,
+  'voice': False, # by now ;)
+  'exposure': True,
+  'transport': ['RFCOMM', 'L2CAP'],
+  'battery': True,
 }
 
+COMMANDS = dict()
+
+
+class Command():
+
+    def __init__(self, name, timeout, command):
+        self.name = name
+        self.timeout = timeout
+        self.command = command
+        COMMANDS[name] = self
+
+Command("date", 6, "D")
+Command("flash", 1, "F")
+Command("size", 3, "S")
+Command("pan", 1, "P")
+Command("exposure", 1, "E")
+
+
 class AIRi(CameraProtocol):
-  client = None
-  callLater = None
-  size = "QVGA"
-  defaults = {
-    "size": "QVGA",
-    "flash": 0,
-    "voice": False,
-    "exposure": 0,
-    "pan": "none",
-  }
+    client = None
+    callLater = None
+    size = "QVGA"
+    defaults = {
+        "size": "QVGA",
+        "flash": 0,
+        "voice": False,
+        "exposure": 0,
+        "pan": "none",
+    }
 
-  def getType(self):
-    return 'AIRi'
+    def getType(self):
+        return 'AIRi'
 
-  def __init__(self, client):
-    self.client = client
-    self.address = client.address
-    self.transport = client.transport
-    self.doSetup()
-  
-  def __doCommand(self, command, value):
-    dbg("doCommand $%s%s" % (command, value))
-    self.transport.write("$%s%s\n\r" % (command, value))
+    def __init__(self, client):
+        self.client = client
+        self.address = client.address
+        self.transport = client.transport
+        self.doSetup()
+        self.pending = []
+        self.callLater = None
 
-  def pushPendingCommands(self):
-    command, value, timeout = self.pending.pop(0)
-    self.__doCommand(command, value)
-    if len(self.pending) > 0:
-      self.callLater = reactor.callLater( timeout, self.pushPendingCommands )
+    def internalDoCommand(self):
+        if len(self.pending) == 0:
+            return
+        command, value, timeout = self.pending.pop(0)
+        if callable(value):
+            value = value()
+        dbg("doCommand $%s%s" % (command, value))
+        self.transport.write("$%s%s\n\r" % (command, value))
+        self.callLater = (timeout, self.internalDoCommand)
 
-  def doSetup(self):
-    dbg("doSetup")
-    nsets = settings.getCamera(self.address)
-    sets = self.defaults
-    size = self.size
-    if nsets:
-      sets.update(nsets)
-    dbg(sets)
-    self.pending = [ ("S", SIZES[sets["size"]], 6),
-      ("F", "0" if not sets["flash"] else "1", 2),
-    ]
-    if sets["pan"]!="none":
-      self.pending.append( ("P", sets["pan"].upper()[0]), 2 )
-    #self.pending.append( ("E", sets["exposure"]) )
-    self.callLater = reactor.callLater( 5, self.pushPendingCommands )
+    def doCommand(self, command, value):
+        if type(command) != Command:
+            command = COMMANDS[command]
+        self.pending.append((command.command, value, command.timeout))
+        if self.callLater and self.callLater.active():
+            return
+        self.internalDoCommand()
 
-  def updateSettings(self):
-    self.doSetup()
+    def doSetup(self):
+        dbg("doSetup")
+        nsets = settings.getCamera(self.address)
+        sets = self.defaults
+        size = self.size
+        if nsets:
+            sets.update(nsets)
+        dbg(sets)
+        self.callLater = reactor.callLater(4, internalDoCommand)
+        self.doCommand("date",
+                        lambda: datetime.utcnow().strftime("%Y%m%dT%H%M%SZ"))
+        self.doCommand("size", SIZES[sets["size"]])
+        self.doCommand("flash", "0" if not sets["flash"] else "1")
+        self.doCommand("pan", sets["pan"].upper()[0])
 
-  def previewData(self):
-    start = self.client.buffer.find("\xff\xd8")
-    end = self.client.buffer.find("\xff\xd9")
-    if start == -1 or end == -1:
-      return
+    def updateSettings(self):
+        self.doSetup()
 
-    frame = self.client.buffer[start:end+1]
-    self.client.buffer = self.client.buffer[end+2:]
-    self.client.gotFrame(frame)
+    def previewData(self):
+        start = self.client.buffer.find("\xff\xd8")
+        end = self.client.buffer.find("\xff\xd9")
+        if start == -1 or end == -1:
+            return
 
-  def dataReceived(self):
-    return self.previewData()
+        frame = self.client.buffer[start:end + 1]
+        self.client.buffer = self.client.buffer[end + 2:]
+        self.client.gotFrame(frame)
 
-  def disconnect(self):
-    dbg("AIRi.disconnect")
+    def dataReceived(self):
+        return self.previewData()
 
-  def set(self, option, value):
-    dbg("set %s-> %s" % (option, value))
-    if option == "size":
-      self.__doCommand("S", SIZES[value])
-    elif option == "flash":
-      self.__doCommand("F", "0" if not value else "1")
-    elif option == "pan":
-      self.__doCommand("P", value.upper()[0])
-    elif option == "exposure":
-      self.__doCommand("E", value)
+    def disconnect(self):
+        dbg("AIRi.disconnect")
+
+    def set(self, option, value):
+        dbg("set %s-> %s" % (option, value))
+        if option == "size":
+            self.doCommand(option, SIZES[value])
+        elif option == "flash":
+            self.doCommand(option, "0" if not value else "1")
+        elif option == "pan":
+            self.doCommand(option, value.upper()[0])
+        elif option == "exposure":
+            self.doCommand(option, value)
 
 AIRi.Capabilities = CAPABILITIES
 AIRi.Sizes = SIZES
