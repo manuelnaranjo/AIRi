10008f7c3b89ded67b9fc7da7c745256ab8be080
Manuel Francisco Naranjo <manuel@aircable.net>
2011-06-27 11:30:22 -0300

    Replaced 2 tabs spaces to 4 spaces

--- python/airi/__init__.py
+++ python/airi/__init__.py
@@ -1,58 +1,59 @@
 # -*- coding: utf-8 -*-
 try:
-    from twisted.internet import glib2reactor
-    glib2reactor.install()
+        from twisted.internet import glib2reactor
+        glib2reactor.install()
 except:
-    pass
+        pass
 
 from twisted.python import log
 import logging
 
-ENTRY   = 10
-ARGS    = 20
+ENTRY       = 10
+ARGS        = 20
 RESULT  = 30
 
 class report:
-  """
-  Decorator that prints information about function calls.
-  Based on: http://paulbutler.org/archives/python-debugging-with-decorators/
-  """
+    """
+    Decorator that prints information about function calls.
+    Based on: http://paulbutler.org/archives/python-debugging-with-decorators/
+    """
 
-  def __init__(self, level=ENTRY, category=None):
-    self.level=level
-    self.category = category
+    def __init__(self, level=ENTRY, category=None):
+        self.level=level
+        self.category = category
 
-  def __call__(self, fn):
-    def wrap(*args, **kwargs):
-      if self.level >= ARGS:
-        fc="call to %s.%s (%s,%s)" % (
-          fn.__module__, fn.__name__,
-          ', '.join( [ repr(a) for a in args ] ),
-          ', '.join( ["%s = %s" % (a, repr(b)) for a,b in kwargs.items()] )
-        )
-        log.msg(fc, level=logging.DEBUG, category=self.category)
-      elif self.level >= ENTRY:
-        log.msg("call to %s.%s" % (fn.__module__, fn.__name__), category=self.category, level=logging.DEBUG)
+    def __call__(self, fn):
+        def wrap(*args, **kwargs):
+            if self.level >= ARGS:
+                fc="call to %s.%s (%s,%s)" % (
+                    fn.__module__, fn.__name__,
+                    ', '.join( [ repr(a) for a in args ] ),
+                    ', '.join( ["%s = %s" % (a, repr(b)) for a,b in kwargs.items()] )
+                )
+                log.msg(fc, level=logging.DEBUG, category=self.category)
+            elif self.level >= ENTRY:
+                log.msg("call to %s.%s" % (fn.__module__, fn.__name__), category=self.category, level=logging.DEBUG)
 
-      ret = fn(*args, **kwargs)
-      if self.level >= RESULT:
-        log.msg("%s returned %s" % (fc, ret), level=logging.DEBUG)
-      return ret
-    if self.level > 0:
-      return wrap
-    return fn
+            ret = fn(*args, **kwargs)
+            if self.level >= RESULT:
+                log.msg("%s returned %s" % (fc, ret), level=logging.DEBUG)
+            return ret
+        if self.level > 0:
+            return wrap
+        return fn
 
 __version__ = ('1', '0', 'c6')
 __version__ = '.'.join(__version__)
 
 try:
-	import camera
-	from camera import dbg, CameraProtocol
-	import settings
-	import twisted_bluetooth
-	import stream
-	import server
-	import api
+    import camera
+    from camera import dbg, CameraProtocol
+    import settings
+    import twisted_bluetooth
+    import stream
+    import server
+    import api
 except Exception, err:
-	log.msg("Failed while importing base modules, ignore if running distutils")
-	log.err(err)
+    log.msg("Failed while importing base modules, ignore if running distutils")
+    log.err(err)
+
--- python/airi/main.py
+++ python/airi/main.py
@@ -5,17 +5,17 @@ from twisted.internet import reactor
 from twisted.python import log
 
 def main():
-	import sys
-	if sys.platform == "linux2" and "-nopairing" not in sys.argv:
-		from pair import main
-		reactor.callWhenRunning(main)
-	log.startLogging(sys.stdout)
-	if len(sys.argv) > 1:
-		jmain(int(sys.argv[1]))
-	else:
-		jmain()
-	reactor.run()
+    import sys
+    if sys.platform == "linux2" and "-nopairing" not in sys.argv:
+        from pair import main
+        reactor.callWhenRunning(main)
+    log.startLogging(sys.stdout)
+    if len(sys.argv) > 1:
+        jmain(int(sys.argv[1]))
+    else:
+        jmain()
+    reactor.run()
 
 if __name__ == '__main__':
-	main()
+    main()
 
