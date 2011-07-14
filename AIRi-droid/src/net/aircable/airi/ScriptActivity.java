3b428f1988e93b3340ef8d29ae5351e375db5547
Manuel Francisco Naranjo <manuel@aircable.net>
2011-07-14 14:31:38 -0300

    Show splash screen while Python is been started

--- AIRi-droid/src/net/aircable/airi/ScriptActivity.java
+++ AIRi-droid/src/net/aircable/airi/ScriptActivity.java
@@ -17,9 +17,12 @@
 package net.aircable.airi;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -34,6 +37,9 @@ import com.googlecode.android_scripting.jsonrpc.RpcReceiverManager;
  */
 public class ScriptActivity extends Activity {
     private static final String TAG = "AIRiActivity";
+    public static final String HIDE_SPLASHSCREEN = "net.aircable.airi.ScriptActivity.HIDE_SPLASHSCREEN";
+    
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +70,30 @@ public class ScriptActivity extends Activity {
             startService(new Intent(this, ScriptService.class));
         } else {
             Log.v(TAG, "NO LAUNCH_SCRIPT_FOR_RESULT");
+            boolean from_notification = this.getIntent().getBooleanExtra(ScriptService.FROM_NOTIFICATION, false); 		
             ScriptApplication application = (ScriptApplication) getApplication();
             if (application.readyToStart()) {
                 startService(new Intent(this, ScriptService.class));
             }
-            finish();
+            if (!from_notification){
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(HIDE_SPLASHSCREEN);
+                this.registerReceiver(new BroadcastReceiver(){
+    				@Override
+    				public void onReceive(Context context, Intent intent) {
+    					if (intent.getAction().equals(HIDE_SPLASHSCREEN)){
+    						Log.v(TAG, "Received " + HIDE_SPLASHSCREEN);
+    						ScriptActivity.this.unregisterReceiver(this);
+    						ScriptActivity.this.mDialog.dismiss();
+    						ScriptActivity.this.finish();
+    					}
+    				}
+                	
+                }, filter);
+            	mDialog = ProgressDialog.show(this, "AIRi", 
+                        "AIRi is Loading. Please wait...", true);
+            } else
+            	this.finish();
         }
     }
 }
